64e1e50faa26b173834a49e01470945e5ad3b03b
MXDragon <bentzbry@gmail.com>
2021-08-23 23:37:55 -0400

    Updates

---
+++ precache-manifest.06ff79337afc48e702a64a8fe0512156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b940b4a95f39050bdc16",
+    "url": "/static/css/main.dfb05ab5.chunk.css"
+  },
+  {
+    "revision": "b940b4a95f39050bdc16",
+    "url": "/static/js/main.b940b4a9.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "68a4d8cc527e75b05c36957a92c9be0f",
+    "url": "/index.html"
+  }
+];
