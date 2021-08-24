92c31a9a26d28a1269c0fe174b8f85ccec9d0f79
MXDragon <bentzbry@gmail.com>
2021-08-24 11:10:50 -0400

    More updates trying to get live server running

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+  "imagebaseurl":"https://MXDragon.github.io/",
+  "name": "Bryan Bentz",
+  "role": "Frontend Developer and Data Scientist",
+  "linkedinId":"bryan-bentz-a2323641",
+  "skypeid": "live:.cid.2aa4fe6140a4242b",
+  "roleDescription": "I play correspondance chess and I raise my awesome 2 year old son",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/bryan-bentz-a2323641/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/MXDragon",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"https://twitter.com/hurondragon",
+        "className":"fa fa-twitter"
+      }
+    ],
+  "aboutme":"Last month of web coding camp, raising my son, have done linux and java dev before",
+  "address":"United States",
+  "website":"https://github.com/MXDragon",
+  "education":[
+    {
+      "UniversityName":"Michigan State University",
+      "specialization":"Kinesiology",
+      "MonthOfPassing":"Aug",
+      "YearOfPassing":"2020",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Michigan State Univertsity Coding",
+      "specialization":"Web Coding: React.js, Node.js",
+      "MonthOfPassing":"Sep",
+      "YearOfPassing":"2021",
+      "Achievements":"Second Brain in class"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Innovative Network Solutions",
+      "specialization":"Wandering SysAdmin",
+      "MonthOfLeaving":"Jul",
+      "YearOfLeaving":"2018",
+      "Achievements":"Worked 2.5 years"
+    },
+    {
+      "CompanyName":"Caregiving/Family",
+      "specialization":"Disabled/Elderly Care",
+      "MonthOfLeaving":"Jul",
+      "YearOfLeaving":"2022",
+      "Achievements":"Cared for disabled mother, elderly grandmother, disabled father and raised son"
+    }
+  ],
+  "skillsDescription":"Javascript, React.js, Node.js, Java, Bash, MERN/LAMP, RESTapi, ",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Covid19 Data Website",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/covid.png"
+    },
+    {
+      "name":"Wanderlog",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/wanderlog.jpg"
+    },
+    {
+      "name":"Password Generator",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/passGen.jpg"
+    },
+    {
+      "name":"WeatherDashboard",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/weatherDash.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"How'd you do that?",
+      "name":"Clueless Administrator"
+    },
+    {
+      "description":"Dada go!",
+      "name":"Sebastian Bentz, my son"
+    }
+  ]
+}
+
+export default resumeData
